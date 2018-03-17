60e5ed645b148a7cca768db16b0877e04c4489f0
Derik Nel <derik.nel@gmail.com>
2018-03-17 20:25:12 +0200

    Added support for Laravel 5.6.*

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,17 +1,55 @@
 <?php
 
-// Menu Routes
-Route::get(Config::get('menuBuilder.config.routes.menu'), ['uses' => 'MenuController@index', 'as' => 'menu.index']);
-Route::get(Config::get('menuBuilder.config.routes.menu') . '/create', ['uses' => 'MenuController@create', 'as' => 'menu.create']);
-Route::get(Config::get('menuBuilder.config.routes.menu') . '/{id}/edit', ['uses' => 'MenuController@edit', 'as' => 'menu.edit']);
-Route::post(Config::get('menuBuilder.config.routes.menu') . '/{id}/edit', ['uses' => 'MenuController@update', 'as' => 'menu.update']);
-Route::post(Config::get('menuBuilder.config.routes.menu') . '/create', ['uses' => 'MenuController@store', 'as' => 'menu.store']);
-Route::get(Config::get('menuBuilder.config.routes.menu') . '/{id}/destroy', ['uses' => 'MenuController@destroy', 'as' => 'menu.destroy']);
+Route::middleware(['web', 'auth'])->group(function () {
+    // Menu Routes
+    Route::get(
+        Config::get('menuBuilder.config.routes.menu'),
+        ['uses' => 'MenuController@index', 'as' => 'menu.index']
+    );
+    Route::get(
+        Config::get('menuBuilder.config.routes.menu') . '/create',
+        ['uses' => 'MenuController@create', 'as' => 'menu.create']
+    );
+    Route::get(
+        Config::get('menuBuilder.config.routes.menu') . '/{id}/edit',
+        ['uses' => 'MenuController@edit', 'as' => 'menu.edit']
+    );
+    Route::post(
+        Config::get('menuBuilder.config.routes.menu') . '/{id}/edit',
+        ['uses' => 'MenuController@update', 'as' => 'menu.update']
+    );
+    Route::post(
+        Config::get('menuBuilder.config.routes.menu') . '/create',
+        ['uses' => 'MenuController@store', 'as' => 'menu.store']
+    );
+    Route::get(
+        Config::get('menuBuilder.config.routes.menu') . '/{id}/destroy',
+        ['uses' => 'MenuController@destroy', 'as' => 'menu.destroy']
+    );
 
-// Menu Group Routes
-Route::get(Config::get('menuBuilder.config.routes.group'), ['uses' => 'GroupController@index', 'as' => 'group.index']);
-Route::get(Config::get('menuBuilder.config.routes.group') . '/create', ['uses' => 'GroupController@create', 'as' => 'group.create']);
-Route::get(Config::get('menuBuilder.config.routes.group') . '/{id}/edit', ['uses' => 'GroupController@edit', 'as' => 'group.edit']);
-Route::post(Config::get('menuBuilder.config.routes.group') . '/{id}/edit', ['uses' => 'GroupController@update', 'as' => 'group.update']);
-Route::post(Config::get('menuBuilder.config.routes.group') . '/create', ['uses' => 'GroupController@store', 'as' => 'group.store']);
-Route::get(Config::get('menuBuilder.config.routes.group') . '/{id}/destroy', ['uses' => 'GroupController@destroy', 'as' => 'group.destroy']);
+    // Menu Group Routes
+    Route::get(
+        Config::get('menuBuilder.config.routes.group'),
+        ['uses' => 'GroupController@index', 'as' => 'group.index']
+    );
+    Route::get(
+        Config::get('menuBuilder.config.routes.group') . '/create',
+        ['uses' => 'GroupController@create', 'as' => 'group.create']
+    );
+    Route::get(
+        Config::get('menuBuilder.config.routes.group') . '/{id}/edit',
+        ['uses' => 'GroupController@edit', 'as' => 'group.edit']
+    );
+    Route::post(
+        Config::get('menuBuilder.config.routes.group') . '/{id}/edit',
+        ['uses' => 'GroupController@update', 'as' => 'group.update']
+    );
+    Route::post(
+        Config::get('menuBuilder.config.routes.group') . '/create',
+        ['uses' => 'GroupController@store', 'as' => 'group.store']
+    );
+    Route::get(
+        Config::get('menuBuilder.config.routes.group') . '/{id}/destroy',
+        ['uses' => 'GroupController@destroy', 'as' => 'group.destroy']
+    );
+});
